137d65871c1904dac8a66276846fa67e3d423ca2
abelmv <mvabel23@gmail.com>
2024-07-22 00:23:23 -0300

    Se corrigieron algunas validaciones y se agrego el servicio para subir archivos multimedia a la nube

--- c19-38-BackEnd/Validaciones/RegistroDtoValidator.cs
+++ c19-38-BackEnd/Validaciones/RegistroDtoValidator.cs
@@ -11,41 +11,28 @@ namespace c19_38_BackEnd.Validaciones
         public RegistroDtoValidator(UserManager<Usuario> userManager)
         {
             _userManager = userManager;
+
             RuleFor(x => x.Nombre)
-                .NotEmpty().WithMessage("El campo Nombre no puede estar vacio")
-                .NotNull().WithMessage("El campo Nombre no puede estar vacio");
+                .NotEmpty().WithMessage("El campo Nombre no puede estar vacio");
 
             RuleFor(x => x.Apellido)
-                .NotEmpty().WithMessage("El campo Apellido no puede estar vacio")
-                .NotNull().WithMessage("El campo Apellido no puede estar vacio");
+                .NotEmpty().WithMessage("El campo Apellido no puede estar vacio");
 
             RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("El campo Email no puede estar vacio")
                 .EmailAddress().WithMessage("Formato de correo invalido")
-                .MustAsync(NoExisteUsuarioConEmail).WithMessage("El correo electronico ya esta en uso");
+                .MustAsync(NoExisteUsuarioConEmail).WithMessage("El correo electronico ya esta en uso")
+                .When(x => !string.IsNullOrEmpty(x.Email) && !string.IsNullOrEmpty(x.Contraseña)); 
 
             RuleFor(x => x.Peso)
-                .NotEmpty().WithMessage("El campo Peso no puede estar vacio")
-                .NotNull().WithMessage("El campo Peso no puede estar vacio")
                 .GreaterThan(0).WithMessage("El campo Peso no puede ser menor o igual a 0");
 
             RuleFor(x => x.Altura)
-                .NotEmpty().WithMessage("El campo Altura no puede estar vacio")
-                .NotNull().WithMessage("El campo Altura no puede estar vacio")
                 .GreaterThan(0).WithMessage("El campo Altura no puede ser menor o igual a 0");
 
             RuleFor(x => x.FechaNacimiento)
-                .Must(fecha =>
-                {
-                    DateTime fechaActual = DateTime.Today;
-                    int edad = fechaActual.Year - fecha.Year;
-
-                    // Verifica si la fecha de nacimiento ya ha ocurrido este año
-                    if (fecha.Date > fechaActual.AddYears(-edad))
-                    {
-                        edad--;
-                    }
-                    return edad >= 18;
-                }).WithMessage("Debe ser mayor de 18 años");
+                .NotEmpty().WithMessage("La fecha de nacimiento no puede estar vacia")
+                .Must(SerMayorDeEdad).WithMessage("Debe ser mayor de 18 años");
 
             RuleFor(x => x.Genero)
                 .IsInEnum().WithMessage("Opcion incorrecta");
@@ -54,14 +41,10 @@ namespace c19_38_BackEnd.Validaciones
                 .IsInEnum().WithMessage("Opcion incorrecta");
 
             RuleFor(x => x.Contraseña)
-               .NotEmpty().WithMessage("El campo Contraseña no puede estar vacio")
-               .NotNull().WithMessage("El campo Contraseña no puede estar vacio");
+               .NotEmpty().WithMessage("El campo Contraseña no puede estar vacio");
 
             RuleFor(x => x.ConfirmarContraseña)
-                .Must((registro, confirmarContraseña) =>
-                {
-                    return confirmarContraseña.Equals(registro.Contraseña);
-                }).WithMessage("Las contraseñas deben coincidir");
+                .Equal(x=>x.Contraseña).WithMessage("Las contraseñas deben coincidir");
 
 
         }
@@ -71,6 +54,18 @@ namespace c19_38_BackEnd.Validaciones
             var result = await _userManager.FindByEmailAsync(email);
 
             return result is null ? true : false;
+        }
+
+        private bool SerMayorDeEdad(DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+
+            if (fechaNacimiento.Date > fechaActual.AddYears(-edad))
+            {
+                edad--;
+            }
+            return edad >= 18;
         }
 
     }
