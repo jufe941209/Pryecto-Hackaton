0f300c4875d6501c37819f93f02b1be68ba8af27
HENRY JULIAN FUENTES CLAVIJO <henry.fuentes101@alu.ulpgc.es>
2024-07-27 03:39:26 -0500

    home + login and routes

--- c19-38-FrontEnd/src/app/app.component.ts
+++ c19-38-FrontEnd/src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { Encuesta1Component } from './components/encuesta1/encuesta1.component';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     Encuesta1Component,
+    HomeComponent,
+    LoginComponent,
+    RouterOutlet,
+   
+
     
-  ],
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'c19-38-FrontEnd';
+ 
 }
