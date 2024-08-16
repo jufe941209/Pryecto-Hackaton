c2be2646f6df2ef4e3e59d7ff279773c239de5cb
HENRY JULIAN FUENTES CLAVIJO <henry.fuentes101@alu.ulpgc.es>
2024-08-16 07:13:43 -0500

    Ya este si quedo

--- findetech-frontend/src/app/app.component.ts
+++ findetech-frontend/src/app/app.component.ts
@@ -4,9 +4,8 @@ import { Encuesta1Component } from './components/encuesta1/encuesta1.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-root',
@@ -18,13 +17,10 @@ import { RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
     SignInComponent,
     RouterOutlet,
     CarouselModule,
-    ReactiveFormsModule,
-    RecaptchaV3Module,
+   
 
 ],
-providers: [
-  { provide: RECAPTCHA_V3_SITE_KEY, useValue: 'YOUR_SITE_KEY' } // Aquí se agrega la clave del sitio
-],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
